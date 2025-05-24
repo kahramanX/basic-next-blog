a9088a2325085a9267324a8d1e2b470d516a7bd1
Kahraman <80291704+kahramanX@users.noreply.github.com>
2025-05-25 02:13:06 +0300

    containers moved to root

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Image from "next/image";
 import "./globals.css";
-import Header from "@/components/containers/Header";
-import Footer from "@/components/containers/Footer";
+import Header from "@/containers/Header";
+import Footer from "@/containers/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
