1f97cb87b1b6c3dae69771acd163a4fb7d9eec4b
Kahraman <80291704+kahramanX@users.noreply.github.com>
2025-05-25 19:57:40 +0300

    blog list updated

--- components/BlogCard/index.tsx
+++ components/BlogCard/index.tsx
@@ -5,7 +5,7 @@ import { Post } from "@/app/types";
 
 export default function BlogCard(props: Post) {
   return (
-    <Link className="flex items-start gap-4 mb-4 " href={`/blog/${props.id}`}>
+    <Link className="flex items-center gap-4 mb-4 " href={`/blog/${props.id}`}>
       <div className="relative max-w-24 w-full h-24 overflow-hidden">
         <Image
           src={`https://picsum.photos/100/100?random=${props.id}`}
@@ -17,6 +17,17 @@ export default function BlogCard(props: Post) {
       <div>
         <div className="text-md font-semibold">{props.title}</div>
         <div className="text-sm text-gray-500 line-clamp-2">{props.body}</div>
+        <div className="flex gap-4">
+          <div className="text-xs text-gray-400 mt-1" title="Views">
+            {props.views || 0} 👁️
+          </div>
+          <div className="text-xs text-gray-400 mt-1" title="Likes">
+            {props.reactions?.like || 0} 👍
+          </div>
+          <div className="text-xs text-gray-400 mt-1" title="Tags">
+            {props.tags.join(", ")} 🏷️
+          </div>
+        </div>
       </div>
     </Link>
   );
